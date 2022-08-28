4128d0b589e3f0887205ef648588c213b355fc7d
ilija98 <ilija691998@gmail.com>
2022-08-28 13:30:28 +0200

    id2 fakultet modul

--- src/app/models/fakultet.ts
+++ src/app/models/fakultet.ts
@@ -1,10 +1,12 @@
-import { StudijskiProgram } from "./studijskiprogram";
+import { StudijskiProgram } from "./studijskiProgram";
 
 
 export interface Fakultet{
     id: number;
+    id2: number;
     naziv: string;
     adresa: string;
     oblast: string;
     strudijskiProgrami: StudijskiProgram;
 }
+
